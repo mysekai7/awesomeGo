59a0f006100a493517b8610baabdc0608f12ec1b
onechao <mysekai7@gmail.com>
2018-04-13 18:30:35 +0800

    comment

--- retriever/main.go
+++ retriever/main.go
@@ -19,7 +19,7 @@ func download(r Retriever) string {
 func main() {
 	var r Retriever
 
-	//值类型
+	//值类型，即可使用值类型也可以使用指针类型
 	r = mock.Retriever{"hello word"}
 	inspect(r)
 
@@ -27,14 +27,14 @@ func main() {
 		retriever/main.go:24:4: cannot use real.Retriever literal (type real.Retriever) as type Retriever in assignment:
 		real.Retriever does not implement Retriever (Get method has pointer receiver)
 	*/
-	//指针类型
+	//指针类型, 指针接受者必须指针类型
 	r = &real.Retriever{
 		UserAgent: "Mozilla/5.0",
 		Timeout:   time.Minute,
 	}
 	inspect(r)
 
-	//Type assertion
+	//Type assertion， 查看指针变量
 	if mockRetriever, ok := r.(mock.Retriever); ok {
 		fmt.Println(mockRetriever.Contents)
 	} else {
