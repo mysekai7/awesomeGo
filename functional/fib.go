c224f4399a953f15ac6902a3cb054f3c6c40c23a
onechao <mysekai7@gmail.com>
2018-04-13 22:23:18 +0800

    闭包/函数式接口

---
+++ functional/fib.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+//1,1,2,3,5,8,13,...
+//  a,b
+//	  a,b
+//斐波那契数列
+func fibonacci() intGen {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+type intGen func() int
+
+//函数式接口
+//函数类型实现Read接口, 函数类型作为接受者
+//Read(p []byte) (n int, err error)
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g() //下一个元素
+	if next > 100 {
+		return 0, io.EOF //读到头
+	}
+	s := fmt.Sprintf("%d\n", next) //转成字符串
+
+	//TODO: p存储太小的问题
+	return strings.NewReader(s).Read(p) //代理read
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+
+	f := fibonacci()
+	//fmt.Println(f()) //1
+	//fmt.Println(f()) //1
+	//fmt.Println(f()) //2
+	//fmt.Println(f()) //3
+	//fmt.Println(f()) //5
+	//fmt.Println(f()) //8
+
+	printFileContents(f)
+
+}
