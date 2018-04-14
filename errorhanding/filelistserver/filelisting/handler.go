37e0be1a73eeef320251a3f2968faad7574775a5
onechao <mysekai7@gmail.com>
2018-04-14 20:50:22 +0800

    错误统一处理 - 错误综合处理

--- errorhanding/filelistserver/filelisting/handler.go
+++ errorhanding/filelistserver/filelisting/handler.go
@@ -4,12 +4,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
 
 func HandleFieldlist(writer http.ResponseWriter,
 	request *http.Request) error {
-
-	path := request.URL.Path[len("/list/"):]
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("Path mast start" +
+			"with" + prefix)
+	}
+	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
 	if err != nil {
 		//panic(err)
--- errorhanding/filelistserver/web.go
+++ errorhanding/filelistserver/web.go
@@ -15,11 +15,28 @@ func errWrapper(handler appHandler) func(
 	http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter,
 		request *http.Request) {
+		//	防止崩溃
+		defer func() {
+			if r := recover(); r != nil {
+				log.Warn("%v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
 		//handler处理业务逻辑
 		err := handler(writer, request)
 		//错误处理
 		if err != nil {
 			log.Warn("Error handling request: %s", err.Error())
+
+			if userErr, ok := err.(userError); ok { //.() Type Assertion判断类型
+				http.Error(writer,
+					userErr.Message(),
+					http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -35,8 +52,14 @@ func errWrapper(handler appHandler) func(
 	}
 }
 
+//自定义错误
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFieldlist))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFieldlist))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
