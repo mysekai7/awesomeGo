7d41e40a73cd5b98335f2f9ce6d901098fe578d9
onechao <mysekai7@gmail.com>
2019-03-28 00:11:57 +0800

    update buffer

--- channel/buffer_chan02/main.go
+++ channel/buffer_chan02/main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
 	"time"
 )
 
 func write(ch chan int) {
-	for i := 0; i < 5; i++ {
-		ch <- i
-		fmt.Println("successfully wrote", i, "to ch")
+	// for i := 0; i < 5; i++ {
+	// 	ch <- i
+	// 	fmt.Println("successfully wrote", i, "to ch")
+	// }
+	// close(ch)
+
+	for {
+		select {
+		case c := <- ch:
+			log.Println("ch: ", c)
+			time.Sleep(1 * time.Second)
+		}
 	}
-	close(ch)
 }
 
 func main() {
-	ch := make(chan int, 2)
+	ch := make(chan int, 100)
 	go write(ch)
-	time.Sleep(2 * time.Second)
-	for v := range ch {
-		fmt.Println("read value", v, "from ch")
-		time.Sleep(2 * time.Second)
+	//time.Sleep(2 * time.Second)
+	// for v := range ch {
+	// 	fmt.Println("read value", v, "from ch")
+	// 	time.Sleep(2 * time.Second)
+	// }
+	for i:=0; i<200; i++ {
+		ch <- i
+		log.Println("i: ", i)
 	}
+	time.Sleep(100 * time.Second)
 }
